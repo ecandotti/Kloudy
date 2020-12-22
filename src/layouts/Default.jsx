257efe1ac1d13588403dd757aa9069c5ea2ce87d
ecandotti <enzo.candotti16@hotmail.com>
2020-12-22 01:33:34 +0100

    remove bubble

--- src/layouts/Default.jsx
+++ src/layouts/Default.jsx
@@ -1,13 +1,11 @@
 import React from 'react'
 
 import Home from '../pages/Home'
-import { Bubble } from '../components/dev/Bubble'
 
 class Default extends React.Component {
   render () {
     return (
       <>
-        <Bubble />
         <Home />
       </>
     )
